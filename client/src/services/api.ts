7ca7cfd2ffdc3cc7bb6d9782377a779b51d15d78
SHIVAM JAISWAR <you@example.com>
2026-07-16 20:51:10 +0530

    Update API URL for deployment

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hospital-management-system-1-57ky.onrender.com",
 });
 
 export default API;
